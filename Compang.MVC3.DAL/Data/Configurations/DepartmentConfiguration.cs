057248b22e74981d2ef019153c45e04afa80527e
NadaEissaa <nadaeissa13@gmail.com>
2025-03-08 07:11:05 +0200

    Implementaion of DAL

---
+++ Compang.MVC3.DAL/Data/Configurations/DepartmentConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Compang.MVC3.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compang.MVC3.DAL.Data.Configurations
+{
+    internal class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id).UseIdentityColumn(10, 10);
+
+                }
+    }
+}
---
+++ Compang.MVC3.DAL/Data/Contexts/CompanyDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Compang.MVC3.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compang.MVC3.DAL.Data.Contexts
+{
+    internal class CompanyDbContext: DbContext
+    {
+
+        public CompanyDbContext() : base()
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server =NADAAEISSAA; Database = Companyy ; Tursted_Connection = True; TrustedServerCertificate = True ");
+
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
